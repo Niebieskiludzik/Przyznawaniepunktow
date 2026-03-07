d39fe18cf835253d2894526457a7debbfe105eda
Niebieskiludzik <minecraftblueboy7@gmail.com>
2026-03-07 11:06:59 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 if (!window.supabaseInitialized) {
   const supabase = window.supabase.createClient(
     'https://wzanqzcjrpbhocrfcciy.supabase.co',
-    'TWÓJ_ANON_KEY'
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind6YW5xemNqcnBiaG9jcmZjY2l5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE0MzQ4MjUsImV4cCI6MjA4NzAxMDgyNX0.VNer3odvLPJzBbecICFZFw86SXvvCbEZDQNVciEm97k'
   );
 
   window.supabaseClient = supabase;
