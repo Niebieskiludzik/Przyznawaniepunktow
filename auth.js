280c6c9fc27a85aa4557e08316e4cab363666196
Niebieskiludzik <minecraftblueboy7@gmail.com>
2026-03-20 18:07:14 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -4,14 +4,6 @@ window.supabaseClient = window.supabase.createClient(
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind6YW5xemNqcnBiaG9jcmZjY2l5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE0MzQ4MjUsImV4cCI6MjA4NzAxMDgyNX0.VNer3odvLPJzBbecICFZFw86SXvvCbEZDQNVciEm97k'
 );
 
-document.addEventListener("DOMContentLoaded", () => {
-  const savedEmail = localStorage.getItem("savedEmail");
-  if(savedEmail){
-    const emailInput = document.getElementById("email");
-    if(emailInput) emailInput.value = savedEmail;
-  }
-});
-
 const supabase = window.supabaseClient; // 👈 używamy tego samego co main.js
 
 // 🔐 LOGIN
