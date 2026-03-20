86098f2130e53e075af393905050f1a54b98d72d
Niebieskiludzik <minecraftblueboy7@gmail.com>
2026-03-20 17:51:22 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 const supabase = window.supabase.createClient(
   'https://wzanqzcjrpbhocrfcciy.supabase.co',
-  'YOUR_ANON_KEY'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind6YW5xemNqcnBiaG9jcmZjY2l5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE0MzQ4MjUsImV4cCI6MjA4NzAxMDgyNX0.VNer3odvLPJzBbecICFZFw86SXvvCbEZDQNVciEm97k'
 );
 
 // 🔐 LOGIN
